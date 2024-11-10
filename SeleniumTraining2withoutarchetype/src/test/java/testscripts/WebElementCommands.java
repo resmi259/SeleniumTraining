f1ec5466a380a5c313a15c2be7afc96c960eeffe
Admin <Admin@192.168.1.39>
2024-11-10 19:28:42 +0530

    Commit_10_Nov

--- SeleniumTraining2withoutarchetype/src/test/java/testscripts/WebElementCommands.java
+++ SeleniumTraining2withoutarchetype/src/test/java/testscripts/WebElementCommands.java
@@ -9,8 +9,57 @@ public class WebElementCommands extends Base {
 	{
 		
 		WebElement showMessageButton = driver.findElement(By.id("button-one"));
+		WebElement classNameSample =driver.findElement(By.className("clearfix"));
+		WebElement nameSample =driver.findElement(By.name("viewport"));
+		WebElement tagNameSample = driver.findElement(By.tagName("header"));
+		WebElement linkTextSample =driver.findElement(By.linkText("Checkbox Demo"));
+		WebElement partialLinkTextSample =driver.findElement(By.partialLinkText("Checkbox"));
+		
+		//using linktext 
+		
+		WebElement selectInput =driver.findElement(By.linkText("Select Input"));
+		WebElement formSubmit =driver.findElement(By.linkText("Form Submit"));
+		WebElement ajaxformsubmit =driver.findElement(By.linkText("Ajax Form Submit"));
+		WebElement radioButton =driver.findElement(By.linkText("Radio Buttons Demo"));
+		WebElement simpleFormDemo =driver.findElement(By.linkText("Simple Form Demo"));
+		//using id
+		
+		WebElement progressBarsid =driver.findElement(By.id("progress-bars"));
+		WebElement listBox =driver.findElement(By.id("list0box"));
+		WebElement alertModals = driver.findElement(By.id("alert-modal"));
+		WebElement others =driver.findElement(By.id("others"));
+		
+	
+		
+		//using partiallink 
+		WebElement jquerySelect2=driver.findElement(By.partialLinkText("Jquery"));
+		WebElement ajaxformsubmit2 =driver.findElement(By.partialLinkText("Ajax"));
+		WebElement simpleFormDemo2 =driver.findElement(By.partialLinkText("Form Demo"));
+		WebElement radioButton2 =driver.findElement(By.partialLinkText("Buttons Demo"));
+
 		
 		
+		driver.navigate().to("https://www.amazon.in/");
+		
+		//By id
+		
+		WebElement searchAmazon = driver.findElement(By.id("nav-search-submit-button"));
+		
+		
+		
+		//By classname
+		WebElement amazonLogo =driver.findElement(By.className("nav-logo-link"));
+		WebElement cartLogo =driver.findElement(By.className("nav-cart-icon"));
+		WebElement flag =driver.findElement(By.className("icp-nav-flag"));
+		//WebElement address =driver.findElement(By.className("nav-line-2 nav-progressive-content"));
+		WebElement home =driver.findElement(By.className("hm-icon-label"));
+		
+		
+		//By partiallink
+		WebElement pressReleases =driver.findElement(By.partialLinkText("Press Releases"));
+		
+		
+		driver.navigate().to("https://selenium.qabible.in/simple-form-demo.php");
 	
 		
 	}
@@ -18,8 +67,7 @@ public class WebElementCommands extends Base {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
- 
-		WebElementCommands webelementcommands =new WebElementCommands();
+ 		WebElementCommands webelementcommands =new WebElementCommands();
 		webelementcommands.intializeBrowser();
 		webelementcommands.webElementCommands();
 		webelementcommands.driverQuit();
